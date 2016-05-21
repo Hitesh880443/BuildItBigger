d2808fa3c4e9add4e9339e416b0b23e234b3beb4
Hitesh <hsutar00@gmail.com>
2016-05-21 11:22:45 +0530

    First commit to git

---
+++ app/src/main/java/com/hitesh/builditbigger/LandingScreen.java
@@ -0,0 +1,13 @@
+package com.hitesh.builditbigger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LandingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_screen);
+    }
+}
